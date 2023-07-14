8494912cf1ef89c8b873ff2ba4ed327107fa0b67
Mayna-B <110005340+Mayna-B@users.noreply.github.com>
2023-07-13 23:50:24 -0400

    Update Pet.js

--- models/Pet.js
+++ models/Pet.js
@@ -1,27 +1,43 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const petSchema = new mongoose.Schema({ 
-    pet_type: {
-      type: String, 
-      enum: ["Dog", "Cat", "Other"]
-    },
-    name: {
-      type: String,
-      required: true
-    },
-    gender: {
-      type: String,
-      enum: ["Male", "Female"]
-    },
-    image: {
-      type: String,
-      default: '/images/Image-Coming-Soon.png'
-    },
-    attribute: {
-      type: String, 
-      required: false
-    }
-})
+const petSchema = new mongoose.Schema({
+  petType: {
+    type: String,
+    enum: ["Dog", "Cat"],
+  },
+  petName: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    enum: ["Male", "Female"],
+  },
+  breed: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: String,
+    required: true
+  },
+  adoptionStatus: {
+    type: String,
+  },
+  image: {
+    type: String,
+    default:
+      "https://e29koex2j9k.exactdn.com/wp-content/uploads/2022/11/cat-placeholder.svg",
+  },
+  introduction: {
+    type: String,
+    required: true
+  },
+  details: {
+    type: String,
+    required: true
+  },
+});
 
-module.exports = mongoose.model('Pet', petSchema)
+module.exports = mongoose.model("Pet", petSchema);
 
